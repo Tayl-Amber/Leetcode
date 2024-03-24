4d2712b8f22fc0f53ca73a91fb0b787ab851682e
TaylAmber <taylamber352@gmail.com>
2024-03-24 22:42:00 +0500

    Create 2951_Find_the_Peaks.cpp

---
+++ 2951_Find_the_Peaks.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> findPeaks(vector<int>& mountain) {
+        std::vector<int> peaks;
+    int n = mountain.size();
+    
+    for (int i = 1; i < n - 1; ++i) {
+        if (mountain[i] > mountain[i-1] && mountain[i] > mountain[i + 1]) {
+            peaks.push_back(i);
+        }
+    }
+    
+    return peaks;
+    }
+};
