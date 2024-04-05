aa544200ea5616ad2e67f3360b6942ad9b62d864
TaylAmber <taylamber352@gmail.com>
2024-04-05 12:03:21 +0500

    Problem 1952 solved

---
+++ 1952_Three_Divisors.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isThree(int n) {
+        int c=0;
+        for(int i=1; i<=n; i++){
+            if(n%i == 0){
+                c++;
+            }
+        }
+     if(c==3){
+        return true;
+     }  
+     return false;
+    }
+};
