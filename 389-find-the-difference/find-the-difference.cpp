e6149ea75e45cbc451fde71d82c0102675d03fed
TaylAmber <taylamber352@gmail.com>
2024-04-09 10:46:21 +0500

    Time: 3 ms (60.66%) | Memory: 7.8 MB (52.33%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        for (size_t i = 0; i < s.size(); ++i) {
+            if (s[i] != t[i]) {
+                return t[i];
+            }
+        }
+        return t.back();
+    }
+};
