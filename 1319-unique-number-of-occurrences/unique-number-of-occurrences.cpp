c4aff43af2d3d397148f38c7d9d3b4a08e16756b
TayalAmber <taylamber352@gmail.com>
2024-05-05 22:50:57 +0500

    Time: 0 ms (100.00%) | Memory: 11.3 MB (20.68%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -4,17 +4,14 @@ public:
          unordered_map<int, int> countMap;
     unordered_set<int> occurrences;
 
-    // Count occurrences of each value in the array
     for (int num : arr) {
         countMap[num]++;
     }
 
-    // Store the counts in a set
     for (const auto& pair : countMap) {
         occurrences.insert(pair.second);
     }
 
-    // Check if the number of occurrences is unique
     return countMap.size() == occurrences.size();
     }
 };
